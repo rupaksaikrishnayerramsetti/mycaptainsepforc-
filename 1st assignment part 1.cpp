8addc1e654f31130fc0e36f743675df1a55776ac
Yerramsetti R Sai Krishna <65356848+rupaksaikrishnayerramsetti@users.noreply.github.com>
2020-09-09 16:51:38 +0530

    Add files via upload

---
+++ 1st assignment part 1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int b;
+char a;
+float c;
+double d;
+cout<<"size of char: "<<sizeof(a)<<endl;
+cout<<"size of int: "<<sizeof(b)<<endl;
+cout<<"size of float: "<<sizeof(c)<<endl;
+cout<<"size of double: "<<sizeof(d)<<endl;
+}
---
+++ 1st assignment part 2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+	//auto a=3.4,b=5.5;
+	float a,b;
+	cout<<"enter two numbers: ";
+	cin>>a>>b;
+	cout<<"product= "<<a*b;
+} 
